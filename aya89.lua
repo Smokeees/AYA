0947484e9302833f63df8c0568febca1cd225758
Smokeees <kanokpol59330@gmail.com>
2025-08-16 23:09:09 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,2 +1,3 @@
 getgenv().V = "Kaitundiamond"
+getgenv().Webhookurl = "https://ptb.discord.com/api/webhooks/1270470782761439315/2xBRTWu0xy4H3gZ0aDARCPJ4Mg8aVAWyCA8FsVGzvieFQUYDdGCZlEyNeMsEc86ZDCPh" --ใส่ลิ้งเว็บฮุก/PasteLinkwebhook
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/Vector_Hub/0/v2"))()
