f866ea1131a60a5c51bc9d6f13d84450bf035b8e
Smokeees <kanokpol59330@gmail.com>
2025-08-19 20:44:59 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,2 +1,10 @@
-getgenv().V = "Kaitundiamond" --Delete if not using kaitun version|ลบออกถ้าไม่ใช้ไก่ตันฟาร์มไดม่อน
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/Vector_Hub/0/v2"))()
+script_key="BVCNTyzEocNdZLFrjNbmLpRQYfiCGaiJ";
+getgenv().Configs = {
+    TurboMode = true, -- โหมดฟาร์มไว (ไม่มากขนาดนั้นอยู่ที่ตัวรัน)
+    WebhookUrl = "", -- ลิ้ง Webhook Discord
+    LimitDiamond = 0, -- จำกัด Diamond ถ้าถึงกำหนดจะหยุดฟาร์ม / ถ้าเป็น 0 คือฟาร์มเรื่อยๆ
+    WebhookOneTime = false, -- ถ้าเป็น true จะแจ้งเฉพาะตอน Diamond ถึงที่กำหนด
+    ServerTimeout = 40, -- กำหนดการหมดเวลายืนในเซิร์ฟ X วิให้ย้าย (ไม่จำเป็นมากเอาไว้กันบัคเท่านั้น)
+    ServerFindRange = 3, -- กำหนดจำนวนเซิร์ฟที่จะหาก่อนย้ายไปยังเซิร์ฟที่หาเจอ
+}
+loadstring(game:HttpGet("https://gist.githubusercontent.com/Clehxb/7ea49f500b6e941f7b6332f6879cd059/raw/99NightLoader"))()
