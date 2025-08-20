c47c5e2cb803c821bc0743d93f92a9840b03405f
Smokeees <kanokpol59330@gmail.com>
2025-08-20 18:23:30 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -6,5 +6,8 @@ getgenv().Configs = {
     WebhookOneTime = false, -- ถ้าเป็น true จะแจ้งเฉพาะตอน Diamond ถึงที่กำหนด
     ServerTimeout = 40, -- กำหนดการหมดเวลายืนในเซิร์ฟ X วิให้ย้าย (ไม่จำเป็นมากเอาไว้กันบัคเท่านั้น)
     ServerFindRange = 3, -- กำหนดจำนวนเซิร์ฟที่จะหาก่อนย้ายไปยังเซิร์ฟที่หาเจอ
+    AutoStronghold = false, -- ออโต้สตรองโฮลด์
+    MaxStrongholdLevel = 3, -- ระดับสูงสุดของสตรองโฮลด์
+    StrongholdFarmTime = 360, -- เวลาฟาร์มในสตรองโฮลด์
 }
 loadstring(game:HttpGet("https://gist.githubusercontent.com/Clehxb/7ea49f500b6e941f7b6332f6879cd059/raw/99NightLoader"))()
