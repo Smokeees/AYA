c4c40971743e694f9494c983dea49c6e2a307a88
Smokeees <kanokpol59330@gmail.com>
2025-08-21 22:37:00 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,13 +1,3 @@
-script_key="BVCNTyzEocNdZLFrjNbmLpRQYfiCGaiJ";
-getgenv().Configs = {
-    TurboMode = true, -- โหมดฟาร์มไว (ไม่มากขนาดนั้นอยู่ที่ตัวรัน)
-    WebhookUrl = "", -- ลิ้ง Webhook Discord
-    LimitDiamond = 0, -- จำกัด Diamond ถ้าถึงกำหนดจะหยุดฟาร์ม / ถ้าเป็น 0 คือฟาร์มเรื่อยๆ
-    WebhookOneTime = false, -- ถ้าเป็น true จะแจ้งเฉพาะตอน Diamond ถึงที่กำหนด
-    ServerTimeout = 40, -- กำหนดการหมดเวลายืนในเซิร์ฟ X วิให้ย้าย (ไม่จำเป็นมากเอาไว้กันบัคเท่านั้น)
-    ServerFindRange = 3, -- กำหนดจำนวนเซิร์ฟที่จะหาก่อนย้ายไปยังเซิร์ฟที่หาเจอ
-    AutoStronghold = false, -- ออโต้สตรองโฮลด์
-    MaxStrongholdLevel = 3, -- ระดับสูงสุดของสตรองโฮลด์
-    StrongholdFarmTime = 360, -- เวลาฟาร์มในสตรองโฮลด์
-}
-loadstring(game:HttpGet("https://gist.githubusercontent.com/Clehxb/7ea49f500b6e941f7b6332f6879cd059/raw/99NightLoader"))()
+_G.OPFarmGem = true
+script_key="JAxjjIyieHEgMpZwsBxezvvyKbOLvZYd";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
