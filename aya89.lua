867874dde50d12efd8ac4d85729a76e60cd07194
Smokeees <kanokpol59330@gmail.com>
2025-08-24 05:35:31 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,12 +1,10 @@
-repeat wait() until game:IsLoaded()
-task.wait(15)
 script_key="BVCNTyzEocNdZLFrjNbmLpRQYfiCGaiJ";
 getgenv().Configs = {
     TurboMode = true, -- โหมดฟาร์มไว (ไม่มากขนาดนั้นอยู่ที่ตัวรัน)
     WebhookUrl = "", -- ลิ้ง Webhook Discord
     LimitDiamond = 0, -- จำกัด Diamond ถ้าถึงกำหนดจะหยุดฟาร์ม / ถ้าเป็น 0 คือฟาร์มเรื่อยๆ
     WebhookOneTime = false, -- ถ้าเป็น true จะแจ้งเฉพาะตอน Diamond ถึงที่กำหนด
-    ServerTimeout = 40, -- กำหนดการหมดเวลายืนในเซิร์ฟ X วิให้ย้าย (ไม่จำเป็นมากเอาไว้กันบัคเท่านั้น)
+    ServerTimeout = 30, -- กำหนดการหมดเวลายืนในเซิร์ฟ X วิให้ย้าย (ไม่จำเป็นมากเอาไว้กันบัคเท่านั้น)
     ServerFindRange = 3, -- กำหนดจำนวนเซิร์ฟที่จะหาก่อนย้ายไปยังเซิร์ฟที่หาเจอ
     AutoStronghold = false, -- ออโต้สตรองโฮลด์
     MaxStrongholdLevel = 3, -- ระดับสูงสุดของสตรองโฮลด์
