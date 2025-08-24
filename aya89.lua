66c4adba5fb5712f0c809e1f7bfb8de7133e594b
Smokeees <kanokpol59330@gmail.com>
2025-08-24 22:47:37 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,5 +1,3 @@
-repeat wait() until game:IsLoaded()
-task.wait(10)
 script_key="BVCNTyzEocNdZLFrjNbmLpRQYfiCGaiJ";
 getgenv().Configs = {
     TurboMode = true, -- โหมดฟาร์มไว (ไม่มากขนาดนั้นอยู่ที่ตัวรัน)
@@ -8,8 +6,8 @@ getgenv().Configs = {
     WebhookOneTime = false, -- ถ้าเป็น true จะแจ้งเฉพาะตอน Diamond ถึงที่กำหนด
     ServerTimeout = 30, -- กำหนดการหมดเวลายืนในเซิร์ฟ X วิให้ย้าย (ไม่จำเป็นมากเอาไว้กันบัคเท่านั้น)
     ServerFindRange = 3, -- กำหนดจำนวนเซิร์ฟที่จะหาก่อนย้ายไปยังเซิร์ฟที่หาเจอ
-    AutoStronghold = false, -- ออโต้สตรองโฮลด์
-    MaxStrongholdLevel = 3, -- ระดับสูงสุดของสตรองโฮลด์
+    AutoStronghold = true, -- ออโต้สตรองโฮลด์
+    MaxStrongholdLevel = 2, -- ระดับสูงสุดของสตรองโฮลด์
     StrongholdFarmTime = 360, -- เวลาฟาร์มในสตรองโฮลด์
 }
 loadstring(game:HttpGet("https://gist.githubusercontent.com/Clehxb/7ea49f500b6e941f7b6332f6879cd059/raw/99NightLoader"))()
