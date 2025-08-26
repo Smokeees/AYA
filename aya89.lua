7e5fa71cd6e54caa1e1270baa8a504cf25492893
Smokeees <kanokpol59330@gmail.com>
2025-08-26 08:48:09 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,8 +1,15 @@
 repeat task.wait() until game:IsLoaded()
 task.wait(20)
-getgenv().LockDiamond = 2000 --ตั้งจำนวนเพชร
-getgenv().LockDiamondEnabled = false --เปิดว่าจะล็อคเพชรไหม
-getgenv().SelectClasses = {"Cyborg"}
-getgenv().BuyClassSelect = false --เปิดว่าจะซื้อคลาสไหม
-getgenv().Webhookurl = "" -- ใส่ลิ้งเว็บฮุก
-loadstring(game:HttpGet('https://raw.githubusercontent.com/AAwful/Vector_Hub/0/1k'))()
+script_key="BVCNTyzEocNdZLFrjNbmLpRQYfiCGaiJ";
+getgenv().Configs = {
+    TurboMode = true, -- โหมดฟาร์มไว (ไม่มากขนาดนั้นอยู่ที่ตัวรัน)
+    WebhookUrl = "", -- ลิ้ง Webhook Discord
+    LimitDiamond = 0, -- จำกัด Diamond ถ้าถึงกำหนดจะหยุดฟาร์ม / ถ้าเป็น 0 คือฟาร์มเรื่อยๆ
+    WebhookOneTime = false, -- ถ้าเป็น true จะแจ้งเฉพาะตอน Diamond ถึงที่กำหนด
+    ServerTimeout = 40, -- กำหนดการหมดเวลายืนในเซิร์ฟ X วิให้ย้าย (ไม่จำเป็นมากเอาไว้กันบัคเท่านั้น)
+    ServerFindRange = 4, -- กำหนดจำนวนเซิร์ฟที่จะหาก่อนย้ายไปยังเซิร์ฟที่หาเจอ
+    AutoStronghold = true, -- ออโต้สตรองโฮลด์
+    MaxStrongholdLevel = 2, -- ระดับสูงสุดของสตรองโฮลด์
+    StrongholdFarmTime = 240, -- เวลาฟาร์มในสตรองโฮลด์
+}
+loadstring(game:HttpGet("https://gist.githubusercontent.com/Clehxb/7ea49f500b6e941f7b6332f6879cd059/raw/99NightLoader"))()
