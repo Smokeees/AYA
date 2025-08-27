78cccb07b9821e2d05b168117f47bbb0105ce488
Smokeees <kanokpol59330@gmail.com>
2025-08-27 22:32:00 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,15 +1,12 @@
-repeat task.wait() until game:IsLoaded()
-task.wait(20)
-script_key="BVCNTyzEocNdZLFrjNbmLpRQYfiCGaiJ";
-getgenv().Configs = {
-    TurboMode = true, -- โหมดฟาร์มไว (ไม่มากขนาดนั้นอยู่ที่ตัวรัน)
-    WebhookUrl = "", -- ลิ้ง Webhook Discord
-    LimitDiamond = 0, -- จำกัด Diamond ถ้าถึงกำหนดจะหยุดฟาร์ม / ถ้าเป็น 0 คือฟาร์มเรื่อยๆ
-    WebhookOneTime = false, -- ถ้าเป็น true จะแจ้งเฉพาะตอน Diamond ถึงที่กำหนด
-    ServerTimeout = 40, -- กำหนดการหมดเวลายืนในเซิร์ฟ X วิให้ย้าย (ไม่จำเป็นมากเอาไว้กันบัคเท่านั้น)
-    ServerFindRange = 4, -- กำหนดจำนวนเซิร์ฟที่จะหาก่อนย้ายไปยังเซิร์ฟที่หาเจอ
-    AutoStronghold = true, -- ออโต้สตรองโฮลด์
-    MaxStrongholdLevel = 2, -- ระดับสูงสุดของสตรองโฮลด์
-    StrongholdFarmTime = 240, -- เวลาฟาร์มในสตรองโฮลด์
+repeat wait() until game:IsLoaded()
+task.wait(10)
+_G.Lock = { -- Cyborg , Assassin , Pyromaniac 
+    "NIL" -- ถ้าใส่ NIL ก็คือไม่ต้องซื้ออาชีพ เก็บเพชรยาวๆ
 }
-loadstring(game:HttpGet("https://gist.githubusercontent.com/Clehxb/7ea49f500b6e941f7b6332f6879cd059/raw/99NightLoader"))()
+_G.AddFriend = true -- เพิ่มเพื่อนออโต้ true / false
+_G.Max_Diamond = 10000 -- เมื่อเพชรครบ 10,000 จะหยุดฟาม
+
+_G.Webhook = "" -- ใส่ลิ้งค์Webhook เพื่อรับการแจ้งเตือนผลการฟามเพชรทุกๆ 300วินาที
+
+script_key = "npBszmvvvdNhXYWYwckIZPYMHDMgBxwx"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/aaa4dc9eee92182acc623f09bc4691ed.lua"))()
