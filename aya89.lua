7c2c3c6c8f8aff55d98b294044366f480ca3f904
Smokeees <kanokpol59330@gmail.com>
2025-08-30 09:14:46 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,8 +1,5 @@
 repeat wait() until game:IsLoaded()
-_G.Webhook = "" -- ใส่ลิ้งค์Webhook เพื่อรับการแจ้งเตือนผลการฟามเพชรทุกๆ 300วินาที
-_G.Lock = { -- Cyborg , Assassin , Pyromaniac 
-"NIL"}
--- ถ้าใส่ NIL ก็คือไม่ต้องซื้ออาชีพ เก็บเพชรยาวๆ
-
-script_key = "umxtMBAydTKVJJpdFwAmFpjSuEGyGcBK"
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/aaa4dc9eee92182acc623f09bc4691ed.lua"))()
+_G.OPFarmGem = true
+-- Script Here !!!
+script_key="JAxjjIyieHEgMpZwsBxezvvyKbOLvZYd";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
