acb42ad18b28fce0b525ac31ac1464440b1dce7f
Smokeees <kanokpol59330@gmail.com>
2025-09-02 23:16:59 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -3,4 +3,4 @@ _G.Lock = { -- Cyborg , Assassin , Pyromaniac
     "Gambler" -- ถ้าใส่ NIL ก็คือไม่ต้องซื้ออาชีพ เก็บเพชรยาวๆ
 }
 script_key = "umxtMBAydTKVJJpdFwAmFpjSuEGyGcBK"
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7407efb3918a89b68e9e6db491fc3c3c.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/aaa4dc9eee92182acc623f09bc4691ed.lua"))()
