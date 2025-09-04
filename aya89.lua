e60a110c760b1b4ca64debcf399d642c02718f1b
Smokeees <kanokpol59330@gmail.com>
2025-09-05 00:33:42 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,8 +1,4 @@
 repeat task.wait() until game:IsLoaded()
-getgenv().AutoFarm = true -- เปิดปิดการฟาร์ม
-getgenv().LockDiamond = 1000 -- ตั้งจำนวนเพชรเป้าหมาย
-getgenv().LockDiamondEnabled = true -- เปิดว่าจะล็อคเพชรไหม
-getgenv().SelectClasses = {"Cyborg"} -- คลาสที่จะซื้อ (สามารถใส่หลายคลาสได้)
-getgenv().BuyClassSelect = false -- เปิดว่าจะซื้อคลาสไหม
-getgenv().Webhookurl = "https://discord.com/api/webhooks/YOUR_WEBHOOK_URL" -- ใส่ลิ้งเว็บฮุก
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/Vector_Hub/refs/heads/0/1kk"))()
+getgenv().V = "Kaitundiamond" --Delete if not using kaitun version|ลบออกถ้าไม่ใช้ไก่ตันฟาร์มไดม่อน
+getgenv().Webhookurl = "LINKWEBHOOK" --Delete if not using Webhook|ลบออกถ้าไม่ใช้เว็ปฮุก
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/Vector_Hub/0/v2"))()
