3d69f28dcd7b2574f4e9ba5d62e3d2d9b5a77362
Smokeees <kanokpol59330@gmail.com>
2025-09-05 18:00:49 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,10 +1,4 @@
 repeat task.wait() until game:IsLoaded()
-_G.Lock = { -- Cyborg , Assassin , Pyromaniac 
-    "Gambler" -- ถ้าใส่ NIL ก็คือไม่ต้องซื้ออาชีพ เก็บเพชรยาวๆ
-}
-_G.AddFriend = true -- เพิ่มเพื่อนออโต้ true / false
-_G.Max_Diamond = 10000 -- เมื่อเพชรครบ 10,000 จะหยุดฟาม
-
-_G.Webhook = "" -- ใส่ลิ้งค์Webhook เพื่อรับการแจ้งเตือนผลการฟามเพชรทุกๆ 300วินาที
-script_key = "umxtMBAydTKVJJpdFwAmFpjSuEGyGcBK"
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/aaa4dc9eee92182acc623f09bc4691ed.lua"))()
+getgenv().V = "Kaitundiamond" --Delete if not using kaitun version|ลบออกถ้าไม่ใช้ไก่ตันฟาร์มไดม่อน
+getgenv().Webhookurl = "LINKWEBHOOK" --Delete if not using Webhook|ลบออกถ้าไม่ใช้เว็ปฮุก
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/Vector_Hub/0/v2"))()
