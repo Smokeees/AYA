40441f976d93810a66725e3911744594a6c97d74
Smokeees <kanokpol59330@gmail.com>
2025-09-07 16:12:35 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,5 +1,22 @@
 
 repeat task.wait() until game:IsLoaded()
+_G.WebhookLink = "LINK HERE"
 _G.OPFarmGem = true
+
+_G.Class = {
+    Enabled = true,
+    Target = "Gambler"
+}
+
+_G.LockDiamond = {
+    Enabled = false,
+    Amount = 10000,
+    SendWebhook = {
+        Enabled = true,
+        WebhookLink = "",
+        Message = "Reached Target", 
+    }
+}
+-- Script Here !!!
 script_key="JAxjjIyieHEgMpZwsBxezvvyKbOLvZYd";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
