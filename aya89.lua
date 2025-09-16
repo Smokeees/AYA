7a9ce349d63c79b6c265a44aff21c4b8c90fb752
Smokeees <kanokpol59330@gmail.com>
2025-09-17 00:06:28 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,21 +1,27 @@
 
 repeat task.wait() until game:IsLoaded()
 _G.WebhookLink = "LINK HERE"
-_G.OPFarmGem = false
+_G.OPFarmGem = true
 
 _G.Class = {
     Enabled = true,
-    Target = "Cyborg" -- Class Name
+    Target = "Gambler" -- Class Name
 }
 
 _G.LockDiamond = {
     Enabled = false,
-    Amount = 1000, -- 1000,2000,3000
+    Amount = 25000, -- 1000,2000,3000
     SendWebhook = {
         Enabled = true,
         WebhookLink = "",
         Message = "Reached Target", 
     }
 }
-script_key="jbCmgxYXYRWFCHpPQmFNgysxuMbadViX";
+-- Log Roblox Account Manger PC ONLY -> TURN ON WEBSERVER IN DEVELOPER MODE AND CHANGE Enabled to true Enabled = true
+_G.LogRam = {
+    Enabled = true,
+    Port = 7963,
+    Debug = false, -- F9 CONSOLE
+}
+script_key="JAxjjIyieHEgMpZwsBxezvvyKbOLvZYd";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
