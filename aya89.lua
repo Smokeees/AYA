d1fdc6783bbccf0666f73b19554ea65ea2328a8f
Smokeees <kanokpol59330@gmail.com>
2025-09-16 07:23:51 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -17,5 +17,5 @@ _G.LockDiamond = {
         Message = "Reached Target", 
     }
 }
-script_key="JAxjjIyieHEgMpZwsBxezvvyKbOLvZYd";
+script_key="uELxKBLMQPnGMuyfdXobAInPxltzYcJz";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
