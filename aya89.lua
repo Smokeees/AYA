16300e8d5f82c78ca98ddb869db2c9fe690bb93a
Smokeees <kanokpol59330@gmail.com>
2025-10-05 07:09:27 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,27 +1,34 @@
 
 repeat task.wait() until game:IsLoaded()
-_G.WebhookLink = "LINK HERE"
-_G.OPFarmGem = true
+script_key="pXIjPOHcdjyPmrzNDVVsTUiGCDomkvos";
+getgenv().Configs = {
+    -- แจ้งเตือน ( Notify )
+    WebhookUrl = "", -- ลิ้ง Webhook Discord
+    WebhookOneTime = false, -- ถ้าเป็น true จะแจ้งเฉพาะตอน Diamond ถึงที่กำหนด
+    DisableLogRAM = true, -- ปิด Log ของ Account Manager
 
-_G.Class = {
-    Enabled = true,
-    Target = "Gambler" -- Class Name
-}
+    -- การฟาร์ม ( Farm )
+    LimitDiamond = 0, -- จำกัด Diamond ถ้าถึงกำหนดจะหยุดฟาร์ม / ถ้าเป็น 0 คือฟาร์มเรื่อยๆ
+    ServerTimeout = 10, -- กำหนดการหมดเวลายืนในเซิร์ฟ X วิให้ย้าย (ไม่จำเป็นมากเอาไว้กันบัคเท่านั้น)
+    MaxRetry = 5, -- นับเวลาถอยหลังสำหรับการหากล่อง
+    ServerFindRange = 1, -- กำหนดจำนวนเซิร์ฟที่จะหาก่อนย้ายไปยังเซิร์ฟที่หาเจอ
+    AutoStronghold = false, -- ออโต้สตรองโฮลด์
+    MaxStrongholdLevel = 1, -- ระดับสูงสุดของสตรองโฮลด์
+    StrongholdFarmTime = 400, -- เวลาฟาร์มในสตรองโฮลด์
 
-_G.LockDiamond = {
-    Enabled = false,
-    Amount = 25000, -- 1000,2000,3000
-    SendWebhook = {
-        Enabled = true,
-        WebhookLink = "",
-        Message = "Reached Target", 
-    }
+    -- คลาส ( Class )
+    BuyClass = {"None"}, -- ชื้อคลาส
+    DoFirstTimeReroll = false, -- รีร้านคลาสครั้งแรกฟรี ถ้าไม่มีคลาสที่ต้องการขาย
+    AutoEquipClass = "Gambler", -- เลือกใส่ Class
+    UpgradeClass = true, -- อัพเวลคลาสอัตโนมัติ (ไม่ได้ทำเควส แค่อัพเวลตอนมันพร้อมเท่านั้น)
+
+    -- เพิ่มเติม ( Misc )
+    ServerHopProtection = true, -- เอาไว้กัน Ronix ไม่ย้ายเซิฟ
+
 }
--- Log Roblox Account Manger PC ONLY -> TURN ON WEBSERVER IN DEVELOPER MODE AND CHANGE Enabled to true Enabled = true
-_G.LogRam = {
-    Enabled = true,
-    Port = 7963,
-    Debug = false, -- F9 CONSOLE
+getgenv().GoogleSheetLog = {
+    -- Google Sheet
+    Enable = false, -- เปิดใช้ฟีเจอร์
+    WebAppURL = "" -- ใส่ URL ของ Google Apps Script ที่เชื่อมต่อกับ Google Sheets ของคุณ
 }
-script_key="JAxjjIyieHEgMpZwsBxezvvyKbOLvZYd";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/Clehxb/7ea49f500b6e941f7b6332f6879cd059/raw/99NightLoader"))()
