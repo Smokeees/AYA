2b17a714141f5616cb14dba2a048f37afdf761d5
Smokeees <kanokpol59330@gmail.com>
2025-10-08 11:18:53 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,11 +1,10 @@
 
-repeat task.wait() until game:IsLoaded()
 _G.WebhookLink = "LINK HERE"
 _G.OPFarmGem = true
 
 _G.Class = {
     Enabled = true,
-    Target = "Gambler" -- Class Name or Target = {"Gambler" , "Cyborg"}
+    Target = "Cyborg" -- Class Name or Target = {"Gambler" , "Cyborg"}
 }
 
 _G.LockDiamond = {
@@ -17,12 +16,8 @@ _G.LockDiamond = {
         Message = "Reached Target", 
     }
 }
--- Log Roblox Account Manger PC ONLY -> TURN ON WEBSERVER IN DEVELOPER MODE AND CHANGE Enabled to true Enabled = true
-_G.LogRam = {
-    Enabled = false,
-    Port = 7963,
-    Debug = false, -- F9 CONSOLE
-}
+-- Log Account Hub link -> https://accounthub.bunnycat.xyz/login
+_G.ApiKey = "" -- get api in link
 -- Script Here !!!
 script_key="JAxjjIyieHEgMpZwsBxezvvyKbOLvZYd";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
