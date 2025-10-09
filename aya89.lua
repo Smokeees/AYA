19c4e81e6eb7aeafa2864a0599c2cb462d5ee53c
Smokeees <kanokpol59330@gmail.com>
2025-10-10 02:44:15 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,23 +1,4 @@
-
-_G.WebhookLink = "LINK HERE"
-_G.OPFarmGem = true
-
-_G.Class = {
-    Enabled = true,
-    Target = "Cyborg" -- Class Name or Target = {"Gambler" , "Cyborg"}
-}
-
-_G.LockDiamond = {
-    Enabled = false,
-    Amount = 1000, -- 1000,2000,3000
-    SendWebhook = {
-        Enabled = true,
-        WebhookLink = "",
-        Message = "Reached Target", 
-    }
-}
--- Log Account Hub link -> https://accounthub.bunnycat.xyz/login
-_G.ApiKey = "" -- get api in link
--- Script Here !!!
+_G.AFKDay = true
+_G.DisableLeave99Days = true
 script_key="JAxjjIyieHEgMpZwsBxezvvyKbOLvZYd";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
