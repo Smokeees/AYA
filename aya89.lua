d2bfefaa9e9934ba0eb7df2e6d28d8a382ed3a8b
Smokeees <kanokpol59330@gmail.com>
2025-10-12 19:20:26 +0700

    Update aya89.lua

--- aya89.lua
+++ aya89.lua
@@ -1,4 +1,5 @@
+setfpscap(5)
 _G.AFKDay = true
-_G.DisableLeave99Days = true
+_G.DisableLeave99Days = false
 script_key="JAxjjIyieHEgMpZwsBxezvvyKbOLvZYd";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
